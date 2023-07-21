33543e868cccbadab0602b552bf77d8dc590baf1
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-21 16:15:49 +0200

    create Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Proton</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proton Store</h1>
+        </Container>
+      </main>
+    </>
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,9 +6,21 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <Navbar>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">Proton Store</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/cart">
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+                <Nav.Link href="/login">
+                  <FaUser /> Login
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar.Toggle>
         </Container>
       </Navbar>
     </header>
