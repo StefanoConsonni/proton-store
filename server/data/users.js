9cbf814d497182c20e4782d4a8092900b16246d5
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-23 11:57:44 +0200

    create seeder.js

--- server/data/users.js
+++ server/data/users.js
@@ -11,11 +11,13 @@ const users = [
     name: "John Doe",
     email: "john@email.com",
     password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
   },
   {
     name: "Jane Doe",
     email: "jane@email.com",
     password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
   },
 ];
 
