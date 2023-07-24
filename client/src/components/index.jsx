73fb4c726edd8550fe4950f579533fd468ac60dc
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-24 13:14:50 +0200

    create cartSlice.js

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as Product } from "./Product";
 export { default as Rating } from "./Rating";
 export { default as Loader } from "./Loader";
+export { default as Message } from "./Message";
