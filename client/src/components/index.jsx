94e756280562c8e52bb5a836c13e6f65d3141966
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-24 12:24:15 +0200

    create Loader.jsx

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Product } from "./Product";
 export { default as Rating } from "./Rating";
+export { default as Loader } from "./Loader";
