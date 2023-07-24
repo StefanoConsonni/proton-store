fff4422f249176db736ea3cfe68b8949863bfdd2
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-24 09:47:54 +0200

    edit HomeScreen.jsx

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,8 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
-export const BASE_URL = ""; // If using proxy
+// If you do not use a proxy in the package.json
+// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
+
+// If you use use a proxy in the package.json
+export const BASE_URL = "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,20 +1,59 @@
 import { Row, Col } from "react-bootstrap";
-import { Product } from "../components";
-import products from "../products";
+import { useParams } from "react-router-dom";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Link } from "react-router-dom";
+import {
+  Product,
+  Loader,
+  Message,
+  Paginate,
+  ProductCarousel,
+  Meta,
+} from "../components";
 
-function HomeScreen() {
+const HomeScreen = () => {
+  const { pageNumber, keyword } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
+
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light mb-4">
+          Go Back
+        </Link>
+      )}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Meta />
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
+      )}
     </>
   );
-}
+};
 
 export default HomeScreen;
