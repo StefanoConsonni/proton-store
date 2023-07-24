b34f0da7bd110c8e337ce83fc420c830540dc037
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-24 18:11:11 +0200

    complete cartUtils.js

--- client/src/index.js
+++ client/src/index.js
@@ -20,6 +20,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/" element={<HomeScreen />} />
     </Route>
   )
 );
