70d72246c40ecdbbc580008c3fec6c3e314a7fea
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-25 19:32:09 +0200

    create authMiddleware.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-// @desc    Auth user & get token
+// @desc    Auth/Login user & get token
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
