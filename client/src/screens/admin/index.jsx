8021ed03f5bc14748a2b1b2a7e02936ed5136a53
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-30 19:39:34 +0200

    complete UserEditScreen.jsx

---
+++ client/src/screens/admin/index.jsx
@@ -0,0 +1,5 @@
+export { default as OrderListScreen } from "./OrderListScreen";
+export { default as ProductEditScreen } from "./ProductEditScreen";
+export { default as ProductListScreen } from "./ProductListScreen";
+export { default as UserListScreen } from "./UserListScreen";
+export { default as UserEditScreen } from "./UserEditScreen";
