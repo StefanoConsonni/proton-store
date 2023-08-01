469abafc220038bee55fdb631d447f910fad423b
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-01 11:12:42 +0200

    edit Product.jsx

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -17,13 +17,10 @@ const Product = ({ product }) => {
         </Link>
 
         <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
 
-        <Card.Text as="h3">€{product.price}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
